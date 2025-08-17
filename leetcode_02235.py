9b802f81d7af8eb906839943e592fd2b98ceeb65
Monica Tsang <babygmono@gmail.com>
2025-08-17 13:54:06 -0400

    Create leetcode_02235.py

---
+++ leetcode_02235.py
@@ -0,0 +1,11 @@
+#2235. Add Two Integers
+#https://leetcode.com/problems/add-two-integers/description/
+#easy
+
+#Given two integers num1 and num2, return the sum of the two integers.
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        new_num = 0
+        new_num = int(num1) + int(num2)
+        return (int(new_num))
